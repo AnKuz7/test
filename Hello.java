9ee58e8a06ce6c0431ed3723a4966f72827cfa65
Anastasia Kuzmina <anastasia.kuzmin@mail.ru>
2020-09-27 15:57:00 +0300

    Added in line whith greeting 2

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 class Hello {
 	public static void main(String[] args) {
                 System.out.println("A");
-		System.out.println("Hello, word!");
+		System.out.println("Hello, word!2");
                 System.out.println("B");
 	}
 }
